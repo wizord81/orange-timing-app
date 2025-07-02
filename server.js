3484fd0ad814f38039707d55e53af08855d0cbae
wizord81 <rswizord@hotmail.fr>
2025-07-02 19:28:56 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+let clients = [];
+
+wss.on('connection', function connection(ws) {
+    clients.push(ws);
+
+    ws.on('message', function incoming(message) {
+        clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        clients = clients.filter(client => client !== ws);
+    });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+server.listen(3000, () => {
+    console.log('Serveur démarré sur http://localhost:3000');
+});
